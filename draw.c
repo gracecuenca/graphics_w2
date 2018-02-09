c23f0ab5f197799f74e481d18ae6dffecab229bf
gracecuenca <gcuenca@stuy.edu>
2018-02-08 21:36:37 -0500

    octant I

--- draw.c
+++ draw.c
@@ -6,5 +6,23 @@
 #include "draw.h"
 
 //Insert your line algorithm here
-void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+//OCTANT I
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c){
+  int x = x0;
+  int y = y0;
+  int A = y1 - y;
+  int B = -(x1 - x);
+  int d = 2*A + B;
+
+  //assume x0 < x1
+  while(x <= x1){
+    plot(s,c,x,y);
+    if(d > 0){
+      y++;
+      d+=2*B;
+    }
+    x++;
+    d+=2*A;
+  }
+
 }
--- main.c
+++ main.c
@@ -11,12 +11,14 @@ int main() {
   screen s;
   color c;
 
-  c.red = 0;
+  c.red = MAX_COLOR;
   c.green = MAX_COLOR;
-  c.blue = 0;
+  c.blue = MAX_COLOR;
 
   clear_screen(s);
 
+  draw_line(50,400,450,400,s,c);
+  draw_line(50,200,450,200,s,c);
 
   display(s);
   save_extension(s, "lines.png");
