b4ffea66109f21ea67cedc526ec555cf3e6da811
gracecuenca <gcuenca@stuy.edu>
2018-02-09 11:56:18 -0500

    DONE w octants

--- draw.c
+++ draw.c
@@ -5,16 +5,81 @@
 #include "display.h"
 #include "draw.h"
 
+//fxn headers
+void oct_one(int x0, int y0, int x1, int y1, screen s, color c);
+void oct_two(int x0, int y0, int x1, int y1, screen s, color c);
+void oct_seven(int x0, int y0, int x1, int y1, screen s, color c);
+void oct_eight(int x0, int y0, int x1, int y1, screen s, color c);
+void vertical(int x0, int y0, int x1, int y1, screen s, color c);
+double slope(int x0, int y0, int x1, int y1);
+
+
 //Insert your line algorithm here
-//OCTANT I
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c){
+  //if the slope is underfined ie. vertical line
+  if(x0 == x1){
+    if(y1 > y0){
+      vertical(x0,y0,x1,y1,s,c);
+    }
+    else{
+      vertical(x1,y1,x0,y0,s,c);
+    }
+  }
+  //normal stuff
+  else{
+    double m = slope(x0,y0,x1,y1);
+    if(x0 < x1){
+      //octI
+      if(m > 0 && m < 1){
+        oct_one(x0,y0,x1,y1,s,c);
+      }
+      //octII
+      else if(m > 1){
+        oct_two(x0,y0,x1,y1,s,c);
+      }
+      //octIII
+      else if(m < 0 && m > -1){
+        oct_eight(x0,y0,x1,y1,s,c);
+      }
+      //octIV
+      else {
+        oct_seven(x0,y0,x1,y1,s,c);
+      }
+    }
+    else{
+      //octV
+      if(m > 0 && m < 1){
+        oct_one(x1,y1,x0,y0,s,c);
+      }
+      //octVI
+      else if(m > 1){
+        oct_two(x1,y1,x0,y0,s,c);
+      }
+      //octIV
+      else if(m < 0 && m > -1){
+        oct_eight(x1,y1,x0,y0,s,c);
+      }
+      //octIII
+      else {
+        oct_seven(x1,y1,x0,y0,s,c);
+      }
+    }
+  }
+}
+
+//SLOPE
+double slope(int x0, int y0, int x1, int y1){
+  return (1.0*(y1-y0))/(1.0*(x1-x0));
+}
+
+//OCTANT I
+void oct_one(int x0, int y0, int x1, int y1, screen s, color c){
   int x = x0;
   int y = y0;
   int A = y1 - y;
   int B = -(x1 - x);
   int d = 2*A + B;
 
-  //assume x0 < x1
   while(x <= x1){
     plot(s,c,x,y);
     if(d > 0){
@@ -23,6 +88,74 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c){
     }
     x++;
     d+=2*A;
+  }
+}
+
+//OCTANT II
+void oct_two(int x0, int y0, int x1, int y1, screen s, color c){
+  int x = x0;
+  int y = y0;
+  int A = y1 - y;
+  int B = -(x1 - x);
+  int d = A + 2*B;
+
+  while(y <= y1){
+    plot(s,c,x,y);
+    if(d < 0){
+      x++;
+      d+=2*A;
+    }
+    y++;
+    d+=2*B;
+  }
+}
+
+//OCTANT VII
+void oct_seven(int x0, int y0, int x1, int y1, screen s, color c){
+  int x = x0;
+  int y = y0;
+  int A = y1 - y;
+  int B = -(x1 - x);
+  int d = A - 2*B;
+
+  while(y >= y1){
+    plot(s,c,x,y);
+    if(d > 0){
+      x++;
+      d+=2*A;
+    }
+    y--;
+    d-=2*B;
+  }
+}
+
+//OCTANT VIII
+void oct_eight(int x0, int y0, int x1, int y1, screen s, color c){
+  int x = x0;
+  int y = y0;
+  int A = y1 - y;
+  int B = -(x1 - x);
+  int d = 2*A - B;
+
+  while(x <= x1){
+    plot(s,c,x,y);
+    if(d < 0){
+      y--;
+      d-=2*B;
+    }
+    x++;
+    d+=2*A;
+  }
+}
+
+//VERTICAL LINE! SEPARATED INTO SEPARATE FXN
+void vertical(int x0, int y0, int x1, int y1, screen s, color c){
+  int x = x0;
+  int y = y0;
+
+  while(y <= y1){
+    plot(s,c,x,y);
+    y++;
   }
 
 }
--- main.c
+++ main.c
@@ -17,8 +17,58 @@ int main() {
 
   clear_screen(s);
 
-  draw_line(50,400,450,400,s,c);
-  draw_line(50,200,450,200,s,c);
+  //draw_line(50,400,450,400,s,c);
+  //draw_line(50,200,450,200,s,c);
+
+  //draw_line(0,0,0,500,s,c);
+  draw_line(0,0,50,500,s,c);
+  draw_line(0,0,100,500,s,c);
+  draw_line(0,0,150,500,s,c);
+  draw_line(0,0,200,500,s,c);
+  draw_line(0,0,250,500,s,c);
+  draw_line(0,0,300,500,s,c);
+  draw_line(0,0,350,500,s,c);
+  draw_line(0,0,400,500,s,c);
+  draw_line(0,0,450,500,s,c);
+  draw_line(0,0,500,500,s,c);
+
+  //draw_line(0,500,0,0,s,c);
+  draw_line(0,500,50,0,s,c);
+  draw_line(0,500,100,0,s,c);
+  draw_line(0,500,150,0,s,c);
+  draw_line(0,500,200,0,s,c);
+  draw_line(0,500,250,0,s,c);
+  draw_line(0,500,300,0,s,c);
+  draw_line(0,500,350,0,s,c);
+  draw_line(0,500,400,0,s,c);
+  draw_line(0,500,450,0,s,c);
+  draw_line(0,500,500,0,s,c);
+
+  //draw_line(0,500,0,0,s,c);
+  draw_line(500,500,450,0,s,c);
+  draw_line(0,500,0,0,s,c);
+  draw_line(0,500,400,0,s,c);
+  draw_line(0,500,200,0,s,c);
+  draw_line(0,500,250,0,s,c);
+  draw_line(0,500,300,0,s,c);
+  draw_line(0,500,350,0,s,c);
+  draw_line(0,500,400,0,s,c);
+  draw_line(0,500,450,0,s,c);
+  draw_line(0,500,500,0,s,c);
+
+  //draw_line(0,500,0,0,s,c);
+  draw_line(0,500,50,0,s,c);
+  draw_line(0,500,100,0,s,c);
+  draw_line(0,500,150,0,s,c);
+  draw_line(0,500,200,0,s,c);
+  draw_line(0,500,250,0,s,c);
+  draw_line(0,500,300,0,s,c);
+  draw_line(0,500,350,0,s,c);
+  draw_line(0,500,400,0,s,c);
+  draw_line(0,500,450,0,s,c);
+  draw_line(0,500,500,0,s,c);
+
+  draw_line(0,0,500,0,s,c);
 
   display(s);
   save_extension(s, "lines.png");
