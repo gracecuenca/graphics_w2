eb77c3034617ea4959b7e7efc0588ca44f98c5c8
gracecuenca <gcuenca@stuy.edu>
2018-02-09 23:05:00 -0500

    xd

--- main.c
+++ main.c
@@ -17,13 +17,70 @@ int main() {
 
   clear_screen(s);
 
-  //draw_line(50,400,450,400,s,c);
-  //draw_line(50,200,450,200,s,c);
+  //rectangle
+  draw_line(50,400,450,400,s,c);
+  draw_line(450,400,450,200,s,c);
+  draw_line(450,200,50,200,s,c);
+  draw_line(50,200,50,400,s,c);
 
+  //square
+  draw_line(260,390,440,390,s,c);
+  draw_line(440,390,440,210,s,c);
+  draw_line(440,210,260,210,s,c);
+  draw_line(260,210,260,390,s,c);
+
+  //T
+  draw_line(80,280,100,280,s,c);
+  draw_line(90,280,90,260,s,c);
+  //h
+  draw_line(105,280,105,260,s,c);
+  draw_line(105,273,115,273,s,c);
+  draw_line(115,273,115,260,s,c);
+  //e
+  draw_line(120,266,130,266,s,c);
+  draw_line(130,266,130,273,s,c);
+  draw_line(130,273,120,273,s,c);
+  draw_line(120,273,120,260,s,c);
+  draw_line(120,260,130,260,s,c);
+
+  //O
+  draw_line(80,250,100,250,s,c);
+  draw_line(100,250,100,230,s,c);
+  draw_line(100,230,80,230,s,c);
+  draw_line(80,230,80,250,s,c);
+  //f
+  draw_line(115,250,105,250,s,c);
+  draw_line(105,250,105,230,s,c);
+  draw_line(105,240,115,240,s,c);
+  //f
+  draw_line(130,250,120,250,s,c);
+  draw_line(120,250,120,230,s,c);
+  draw_line(120,240,130,240,s,c);
+  //i
+  draw_line(135,240,135,230,s,c);
+  draw_line(135,246,135,243,s,c);
+  //c
+  draw_line(150,245,140,245,s,c);
+  draw_line(140,245,140,230,s,c);
+  draw_line(140,230,150,230,s,c);
+  //e
+  draw_line(155,245,165,245,s,c);
+  draw_line(165,245,165,238,s,c);
+  draw_line(165,238,155,238,s,c);
+  draw_line(155,245,155,230,s,c);
+  draw_line(155,230,165,230,s,c);
+
+  //standing dude
+  draw_line(300,370,320,370,s,c);
+  draw_line(320,370,320,350,s,c);
+  draw_line(320,350,300,350,s,c);
+  draw_line(300,350,300,370,s,c);
+
+  //COBWEBS
+  /***
   int change = 50;
   int count = 0;
 
-  //first shootout
   while(count < 10){
     draw_line(0,0,change,500,s,c);
     change+=50;
@@ -56,6 +113,7 @@ int main() {
     change+=50;
     count++;
   }
+  ***/
 
   display(s);
   save_extension(s, "lines.png");
